04c171d128ae5bf8284b6de753092822bd5ad5c2
yeohwanyun <cwss1503@gmail.com>
2018-06-19 20:09:19 +0900

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,11 +9,25 @@
       </div>
       <div class="navbar-menu is-active">
         <div class="navbar-end">
-          <nuxt-link to="/stat" class="navbar-item">통계</nuxt-link>
-          <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
         </div>
       </div>
     </nav>
+  <div class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <figure class="image is-250x250"></figure>
+          <h1 class="title is-1">YUN_Portfolio</h1>
+          <h2 class="subtitle">윤여환의 포트폴리오입니다.</h2>
+          <br>
+           <nuxt-link to="/"><a class="button is-primary is-inverted is-outlined">자기소개</a></nuxt-link>
+          <nuxt-link to="/work"> <a class="button is-primary is-inverted is-outlined">좋아하는 것</a></nuxt-link>
+          <nuxt-link to="/stat"><a class="button is-link is-inverted is-outlined">학점</a></nuxt-link>
+    </div>
+    <br>
+  
+      </div>
+    </div>
+    
     <nuxt/>
 
     <footer class="footer has-text-centered">
