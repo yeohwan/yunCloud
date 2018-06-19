9d318cbb6eeb288372e8fe0d08d021d28ed20dc8
yeohwanyun <cwss1503@gmail.com>
2018-06-19 20:30:56 +0900

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    
+    <div class="hero is-dark" style="margin-right :25px; margin-left :25px;">
+-      <div class="hero-body">
+-        <div class="container">
+-          <figure class="image is-250x250"></figure>
+-          <h1 class="title is-1">YUN_Portfolio</h1>
+-          <h2 class="subtitle">윤여환의 포트폴리오입니다.</h2>
+-          <br>
+-   </div>
+-      </div>
+-    </div>
+
     <nav class="navbar">
       <div class="navbar-brand">
         <nuxt-link to="/" class="navbar-item"></nuxt-link>
