46a5d518a913cb6d2a158195990cc9b5a3407f04
yeohwanyun <cwss1503@gmail.com>
2018-06-19 20:21:00 +0900

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
-    <nav class="navbar">
-      <div class="navbar-brand">
-        <nuxt-link to="/" class="navbar-item">개사랑</nuxt-link>
-            <figure class="image is-128x128">
-            <img src="~static/dogfoot.jpg" alt="">
-          </figure>
-      </div>
-      <div class="navbar-menu is-active">
-        <div class="navbar-end">
-        </div>
-      </div>
-    </nav>
-  <div class="hero is-dark">
+     <div class="hero is-dark" style="margin-right :25px; margin-left :25px;">
       <div class="hero-body">
         <div class="container">
           <figure class="image is-250x250"></figure>
           <h1 class="title is-1">YUN_Portfolio</h1>
           <h2 class="subtitle">윤여환의 포트폴리오입니다.</h2>
           <br>
-           <nuxt-link to="/"><a class="button is-primary is-inverted is-outlined">자기소개</a></nuxt-link>
-          <nuxt-link to="/work"> <a class="button is-primary is-inverted is-outlined">좋아하는 것</a></nuxt-link>
-          <nuxt-link to="/stat"><a class="button is-link is-inverted is-outlined">학점</a></nuxt-link>
-    </div>
-    <br>
-  
+   </div>
       </div>
     </div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item"></nuxt-link>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end" style="margin-right : 30px;">
+          
+           <nuxt-link to="/"><a class="button is-dark" style="margin : 10px; width : 150px; height : 50px;">자기소개</a></nuxt-link>
+          <nuxt-link to="/work"> <a class="button is-dark" style="margin : 10px; width : 150px; height : 50px;">좋아하는 것</a></nuxt-link>
+          <nuxt-link to="/stat"><a class="button is-dark" style="margin : 10px; width : 150px; height : 50px;">학점</a></nuxt-link>
+           
+        </div>
+      </div>
+    </nav>
     
+    
+     
+<br>
+
+<br>
     <nuxt/>
 
     <footer class="footer has-text-centered">
@@ -38,3 +40,6 @@
     </footer>
   </div>
 </template>
+
+<style>
+</style>
